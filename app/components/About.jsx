9730975f67ff579a0c39987f9a9de760b6701c61
Perry Anderson <perry.anderson@gmail.com>
2017-01-03 20:19:19 -0330

    Updated About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,28 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the about page</p>
+            <h1 className="text-center">About</h1>
+            <p>
+                This is a tutorial project used for demonstrating
+                how to properly utilize React and Redux inside a Webpack
+                configured project using Foundation on a NodeJS server.
+            </p>
+            <p>
+                Here are some of the tools I used:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React </a>
+                    - This was the JavaScript framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map </a>
+                    - I used Open Weather Map to search for weather data by city name.
+                </li>
+            </ul>
         </div>
     );
 }
